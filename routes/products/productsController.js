d8923931dcd8e665d56393c91da795f3a935dee8
Alan Sanchez <alsans81@outlook.com>
2026-02-09 22:17:02 -0500

    Updated routes and controller

--- routes/products/productsController.js
+++ routes/products/productsController.js
@@ -3,11 +3,21 @@ const Product = require('../products/productsModel')
 // Get Product
 const getAllProducts = async (productData) => {
     try {
-        const findProducts = await Product.find();
+        const findProducts = await Product.find(productData);
         return findProducts
     } catch (error) {
-        throw new Error(error);
+        throw error;
 
+    }
+}
+
+// Get Product by ID
+const getProductById = async (productID) => {
+    try {
+        const itemId = await Product.findById(productID)
+        return itemId
+    } catch (error) {
+        throw error;
     }
 }
 
@@ -17,11 +27,27 @@ const createProduct = async (productData) => {
         const newProduct = await Product.create(productData);
         return newProduct
     } catch (error) {
-        throw new Error(error);
+        throw error;
+    }
+}
+
+// Update Product by Id
+const updateProductById = async (productId, productData) => {
+    try {
+        const updateProduct = await Product.findByIdAndUpdate(
+            productId,
+            productData,
+            { new: true }
+        )
+        return updateProduct
+    } catch (error) {
+        throw error;
     }
 }
 
 module.exports = {
+    getAllProducts,
+    getProductById,
     createProduct,
-    getAllProducts
+    updateProductById
 }
--- routes/products/productsRouter.js
+++ routes/products/productsRouter.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { getAllProducts, createProduct } = require('../products/productsController');
+const { getAllProducts, createProduct, getProductById, updateProductById } = require('../products/productsController');
 
 // Get all products
 router.get('/', async (request, response) => {
@@ -13,11 +13,31 @@ router.get('/', async (request, response) => {
     }
 })
 
+// Get Product by ID
+router.get('/:productId', async (request, response) => {
+    try {
+        const productId = await getProductById(request.params.productId)
+        response.status(200).json({ message: 'success', payload: productId })
+    } catch (error) {
+        response.status(404).json({ message: 'failure', payload: error })
+    }
+})
+
 // Create a new product
 router.post('/', async (request, response) => {
     try {
         const newProducts = await createProduct(request.body);
-        response.status(200).json({ message: 'success', payload: newProducts })
+        response.status(201).json({ message: 'success', payload: newProducts })
+    } catch (error) {
+        response.status(400).json({ message: 'failure', payload: error })
+    }
+})
+
+// Update the product by it's id
+router.put('/:productId', async (request, response) => {
+    try {
+        const updateProduct = await updateProductById(request.params.productId, request.body)
+        response.status(200).json({ message: 'success', payload: updateProduct })
     } catch (error) {
         response.status(400).json({ message: 'failure', payload: error })
     }
