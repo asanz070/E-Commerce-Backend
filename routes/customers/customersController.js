e9445e61f6c02fdb42d850302c9dea79a082d732
Alan Sanchez <alsans81@outlook.com>
2026-02-10 00:49:21 -0500

    Completed Crud Operations for Customer

--- routes/customers/customersController.js
+++ routes/customers/customersController.js
@@ -1,9 +1,63 @@
-const Customer = require('./customersModel');
+const Customer = require('../customers/customersModel');
 
+// Get Customer
+const getAllCustomer = async (customerData) => {
+    try {
+        const findCustomer = await Customer.find(customerData);
+        return findCustomer
+    } catch (error) {
+        throw error
+    }
+}
+
+// Get customer by ID
+const getCustomerById = async (customerId) => {
+    try {
+        const customerId = await Customer.findById(customerId)
+        return customerId
+    } catch (error) {
+        throw error
+    }
+}
+
+// Create the customer
 const createCustomer = async (customerData) => {
     try {
-        
+        const newCustomer = await Customer.create(customerData)
+        return newCustomer
     } catch (error) {
-        
+        throw error
     }
+}
+
+// Update the customer by id
+const updateCustomerById = async (customerId, customerData) => {
+    try {
+        const updateCustomer = await Customer.findByIdAndUpdate(
+            customerId,
+            customerData,
+            { new: true }
+        )
+        return updateCustomer
+    } catch (error) {
+        throw error
+    }
+}
+
+// Delete the customer by id
+const deleteCustomerById = async (customerId) => {
+    try {
+        const deleteCustomer = await Customer.findByIdAndDelete(customerId)
+        return deleteCustomer
+    } catch (error) {
+        throw error
+    }
+}
+
+module.exports = {
+    getAllCustomer,
+    getCustomerById,
+    createCustomer,
+    updateCustomerById,
+    deleteCustomerById
 }
---
+++ routes/customers/customersRouter.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+
+const { getAllCustomer, createCustomer, updateCustomerById } = require('./customersController');
+
+// GET All Customer
+router.get('/', async (request, response) => {
+    try {
+        const getCustomer = await getAllCustomer(request.query);
+        response.status(200).json({ message: 'success', payload: getCustomer })
+    } catch (error) {
+        response.status(400).json({ message: 'failure', payload: error })
+    }
+})
+
+// Get Customer by ID
+router.get('/:customerId', async (request, response) => {
+    try {
+        const customerId = await getCustomerById(request.params.customerId)
+        response.status(200).json({ message: 'success', payload: customerId })
+    } catch (error) {
+        response.status(404).json({ message: 'failure', payload: error })
+    }
+})
+
+// Create a new Customer
+router.post('/', async (request, response) => {
+    try {
+        const newCustomer = await createCustomer(request.body);
+        response.status(201).json({ message: 'success', payload: newCustomer })
+    } catch (error) {
+        response.status(400).json({ message: 'failure', payload: error })
+    }
+})
+
+// Update Customer by ID
+router.put('/:customerId', async (request, response) => {
+    try {
+        const updateCustomer = await updateCustomerById(request.params.customerId)
+        response.status(200).json({ message: 'success', payload: updateCustomer })
+    } catch (error) {
+        response.status(400).json({ message: 'failure', payload: error })
+    }
+})
+
+module.exports = router
