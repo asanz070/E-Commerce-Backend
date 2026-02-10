b24dc545d22fe26ad570e37d5ce460b5b32004cb
Alan Sanchez <alsans81@outlook.com>
2026-02-09 20:19:58 -0500

    Added the routes for each individual category

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 // ECHO is on.
 const express = require('express');
 const logger = require('morgan');
-const connectToMongoDB = require('./database/connectToMongoDB')
-
 const app = express();
+
+const connectToMongoDB = require('./database/connectToMongoDB')
 
 const PORT = 3000;
 
+const dotenv = require('dotenv');
+dotenv.config();
+
+// MiddleWare
 app.use(express.json())
 app.use(logger('dev'))
+
+// Routes
+const productRouter = require('./routes/products/productsRouter');
+app.use('/api/products', productRouter)
 
 app.listen(PORT, () => {
     console.log(`Server is listening on PORT: ${PORT}`);
