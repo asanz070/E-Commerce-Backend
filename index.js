7f03a3867d93f60b13625e0d541260024db78800
Alan Sanchez <alsans81@outlook.com>
2026-02-12 20:35:26 -0500

    Added cart route to index file

--- index.js
+++ index.js
@@ -15,11 +15,17 @@ app.use(express.json())
 app.use(logger('dev'))
 
 // Routes
+// Products
 const productRouter = require('./routes/products/productsRouter');
 app.use('/api/products', productRouter)
 
+// Customers
 const customerRouter = require('./routes/customers/customersRouter');
 app.use('/api/customer', customerRouter)
+
+// Carts
+const cartRouter = require('./routes/shopping-carts/cartsRouter')
+app.use('/api/carts')
 
 app.listen(PORT, () => {
     console.log(`Server is listening on PORT: ${PORT}`);
