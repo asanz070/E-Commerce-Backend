aa5d027572a30bcafca5433b5b847628debd926f
Alan Sanchez <alsans81@outlook.com>
2026-02-13 20:12:53 -0500

    orderController boilerplate setup

---
+++ routes/orders/ordersController.js
@@ -0,0 +1,39 @@
+const Order = require('../orders/ordersModel');
+
+// Create customer order
+const placeOrder = async (customerId) => {
+    try {
+
+    } catch (error) {
+        throw error
+    }
+}
+
+// Get orders for one customer
+const getCustomerOrders = async (customerId, status) => {
+    try {
+
+    } catch (error) {
+        throw error
+    }
+}
+
+// Get one specific order for customer
+const getOrderById = async (orderId) => {
+    try {
+
+    } catch (error) {
+        throw error
+    }
+}
+
+// Update order status
+const updateOrderStatus = async (orderId, newStatus) => {
+    try {
+
+    } catch (error) {
+        throw error
+    }
+}
+
+module.exports = {}
